7b48e77abee9b7c67931881e53dd11369780398e
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2020-01-19 16:05:27 +0100

    remove superflous code

--- app/src/main/java/com/maruiz/composeworkshop/domain/di/module.kt
+++
@@ -1,8 +0,0 @@
-package com.maruiz.composeworkshop.domain.di
-
-import com.maruiz.composeworkshop.domain.usecases.GetBooks
-import org.koin.dsl.module
-
-val domainModule = module {
-    factory { GetBooks(get()) }
-}
--- app/src/main/java/com/maruiz/composeworkshop/domain/usecases/GetBooks.kt
+++ app/src/main/java/com/maruiz/composeworkshop/domain/usecases/GetBooks.kt
@@ -1,22 +1,12 @@
 package com.maruiz.composeworkshop.domain.usecases
 
 import arrow.core.Either
+import arrow.core.Right
 import com.maruiz.composeworkshop.data.error.Failure
-import com.maruiz.composeworkshop.data.repository.BookRepository
 import com.maruiz.composeworkshop.domain.model.BookModelDomainModel
 
-class GetBooks(private val bookRepository: BookRepository) :
+class GetBooks() :
     UseCase<List<BookModelDomainModel>, Unit>() {
     override suspend fun run(params: Unit): Either<Failure, List<BookModelDomainModel>> =
-        bookRepository.getBooks().map { success ->
-            success.map {
-                BookModelDomainModel(
-                    it.title,
-                    it.author,
-                    it.shortSynopsis,
-                    it.synopsis,
-                    it.image
-                )
-            }
-        }
+        Right(listOf(BookModelDomainModel()))
 }
