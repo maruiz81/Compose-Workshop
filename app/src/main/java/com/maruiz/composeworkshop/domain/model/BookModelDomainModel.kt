c3eb87af31634856d20a07a14065cfc16905c245
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2020-01-26 00:10:16 +0100

    Add initial preloaded books

--- app/src/main/java/com/maruiz/composeworkshop/domain/model/BookModelDomainModel.kt
+++ app/src/main/java/com/maruiz/composeworkshop/domain/model/BookModelDomainModel.kt
@@ -1,6 +1,10 @@
 package com.maruiz.composeworkshop.domain.model
 
 class BookModelDomainModel(
-    val title: String = "", val author: String = "", val shortSynopsis: String = "",
-    val synopsis: String = "", val image: String = ""
+    val title: String = "",
+    val author: String = "",
+    val date: String,
+    val shortSynopsis: String = "",
+    val synopsis: String = "",
+    val image: String = ""
 )
--- app/src/main/java/com/maruiz/composeworkshop/presentation/view/MainActivity.kt
+++ app/src/main/java/com/maruiz/composeworkshop/presentation/view/MainActivity.kt
@@ -2,11 +2,6 @@ package com.maruiz.composeworkshop.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
-import androidx.ui.core.Text
-import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
-import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
