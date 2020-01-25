74ce2019198f1479c4d8d72fce48c22ecc9b0d2b
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2020-01-25 23:10:36 +0100

    Add first view column Title, author and date

--- app/src/main/java/com/maruiz/composeworkshop/presentation/presentationmodel/BookPresentationModel.kt
+++ app/src/main/java/com/maruiz/composeworkshop/presentation/presentationmodel/BookPresentationModel.kt
@@ -5,6 +5,10 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class BookPresentationModel(
-    val title: String = "", val author: String = "", val shortSynopsis: String = "",
-    val synopsis: String = "", val image: String = ""
+    val title: String = "",
+    val author: String = "",
+    val date: String = "",
+    val shortSynopsis: String = "",
+    val synopsis: String = "",
+    val image: String = ""
 ) : Parcelable
--- app/src/main/java/com/maruiz/composeworkshop/presentation/view/MainActivity.kt
+++ app/src/main/java/com/maruiz/composeworkshop/presentation/view/MainActivity.kt
@@ -1,31 +1,62 @@
 package com.maruiz.composeworkshop.presentation.view
 
+import BooksViewModel
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.lifecycle.Observer
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
-import androidx.ui.graphics.Color
-import androidx.ui.text.TextStyle
+import androidx.ui.layout.Column
 import androidx.ui.tooling.preview.Preview
+import com.maruiz.composeworkshop.presentation.presentationmodel.BookPresentationModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val booksViewModel: BooksViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Hello()
+            PaintBooks(emptyList())
         }
+
+        getBooks()
     }
 
     @Composable
-    fun Hello() {
-        Text("Hello, World!", style = TextStyle(color = Color.Blue))
+    fun PaintBooks(books: List<BookPresentationModel>) {
+        Column() {
+            books.forEach {
+                Text(it.title)
+                Text(it.author)
+                Text(it.date)
+            }
+        }
     }
 
     @Preview
     @Composable
     fun preview() {
-        Hello()
+        PaintBooks(previewBook())
     }
+
+    private fun getBooks() {
+        booksViewModel.run {
+            observeBooks().observe(this@MainActivity, Observer {
+                PaintBooks(it)
+            })
+            requestBooks()
+        }
+    }
+
+    private fun previewBook() =
+        listOf(
+            BookPresentationModel(
+                "Don Quixote", "Miguel de Cervantes",
+                "1605"
+            )
+        )
 }
