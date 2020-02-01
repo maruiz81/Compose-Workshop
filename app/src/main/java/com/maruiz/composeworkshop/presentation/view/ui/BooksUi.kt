330d38b514a4eb692d355c883039404585580a27
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2020-02-01 21:10:05 +0100

    Added material theme and use some text style for Title, author and date

--- app/src/main/java/com/maruiz/composeworkshop/presentation/view/ui/BooksUi.kt
+++ app/src/main/java/com/maruiz/composeworkshop/presentation/view/ui/BooksUi.kt
@@ -4,6 +4,7 @@ import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutPadding
+import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
 import com.maruiz.composeworkshop.presentation.presentationmodel.BookPresentationModel
@@ -11,12 +12,15 @@ import com.maruiz.composeworkshop.presentation.view.states.BooksState
 
 @Composable
 fun PaintBooks(booksState: BooksState) {
-    Column {
-        booksState.books.forEach {
-            Column(LayoutPadding(16.dp)) {
-                Text(it.title)
-                Text(it.author)
-                Text(it.date)
+    MaterialTheme {
+        val typography = MaterialTheme.typography()
+        Column {
+            booksState.books.forEach {
+                Column(LayoutPadding(16.dp)) {
+                    Text(it.title, style = typography.h5)
+                    Text(it.author, style = typography.h6)
+                    Text(it.date, style = typography.body1)
+                }
             }
         }
     }
