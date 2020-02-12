91a0ea5358a1fe1207a2a57de9ca2ec4b49077cd
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2020-02-12 22:22:49 +0100

    Added date in presentation model and remove unneeded parcelable

--- app/src/main/java/com/maruiz/composeworkshop/presentation/presentationmodel/BookPresentationModel.kt
+++ app/src/main/java/com/maruiz/composeworkshop/presentation/presentationmodel/BookPresentationModel.kt
@@ -1,10 +1,11 @@
 package com.maruiz.composeworkshop.presentation.presentationmodel
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class BookPresentationModel(
-    val title: String = "", val author: String = "", val shortSynopsis: String = "",
-    val synopsis: String = "", val image: String = "", val genres: List<String>
-) : Parcelable
+    val title: String = "",
+    val author: String = "",
+    val date: String = "",
+    val shortSynopsis: String = "",
+    val synopsis: String = "",
+    val image: String = "",
+    val genres: List<String>
+)
