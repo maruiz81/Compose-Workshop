d9f656ca13bdfdf73adbc406577ccd99ec72d36e
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2020-02-13 22:40:43 +0100

    Added Genres section

--- app/src/main/java/com/maruiz/composeworkshop/presentation/view/ui/BooksUi.kt
+++ app/src/main/java/com/maruiz/composeworkshop/presentation/view/ui/BooksUi.kt
@@ -2,11 +2,11 @@ package com.maruiz.composeworkshop.presentation.view.ui
 
 import androidx.compose.Composable
 import androidx.ui.core.Text
-import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutPadding
 import androidx.ui.layout.LayoutWidth
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Typography
 import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
@@ -19,15 +19,33 @@ fun PaintBooks(booksState: BooksState) {
         val typography = MaterialTheme.typography()
         Column {
             booksState.books.forEach {
-                Surface {
-                    Column(modifier = LayoutPadding(16.dp) + LayoutWidth.Fill) {
-                        Text(it.title, style = typography.h5)
-                        Text(it.author, style = typography.h6)
-                        Text(it.date, style = typography.body1)
-                    }
-                }
+                MainSection(it.title, it.author, it.date, typography)
+                GenresSection(it.genres, typography)
             }
         }
+    }
+}
+
+@Composable
+private fun MainSection(
+    title: String,
+    author: String,
+    date: String,
+    typography: Typography
+) {
+    Surface {
+        Column(modifier = LayoutPadding(16.dp) + LayoutWidth.Fill) {
+            Text(title, style = typography.h5)
+            Text(author, style = typography.h6)
+            Text(date, style = typography.body1)
+        }
+    }
+}
+
+@Composable
+private fun GenresSection(genres: List<String>, typography: Typography) {
+    Column(modifier = LayoutPadding(16.dp)) {
+        genres.forEach { Text(text = it, style = typography.body1) }
     }
 }
 
@@ -45,6 +63,6 @@ private fun previewBook() =
     listOf(
         BookPresentationModel(
             "Don Quixote", "Miguel de Cervantes",
-            "1605"
+            "1605", genres = listOf("genre 1", "genre 2")
         )
     )
